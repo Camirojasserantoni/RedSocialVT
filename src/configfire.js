cdb510c8d2ea45f76f630819bd7aff2ed7fb0972
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-02-06 13:07:39 -0300

    inicializando firebase en el proyecto

---
+++ src/configfire.js
@@ -0,0 +1,19 @@
+
+
+export const fireInit = () => {
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBsLKIQsSJxJkMDZTVvABXToT7ftLbOqWc',
+    authDomain: 'sketchbook-bcd7a.firebaseapp.com',
+    databaseURL: 'https://sketchbook-bcd7a.firebaseio.com',
+    projectId: 'sketchbook-bcd7a',
+    storageBucket: 'sketchbook-bcd7a.appspot.com',
+    messagingSenderId: '491735262369',
+    appId: '1:491735262369:web:95d163e333e60af871bce0',
+    measurementId: 'G-S16NW4NF64',
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+};
